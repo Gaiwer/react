c9833a8a85181b7a517233799f5c50aee9a7b972
Alexander Oborin <gaiwerspace@gmail.com>
2018-01-21 22:46:09 +0200

    Initial commit of the project

---
+++ src/TodoList.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import TodoItems from "./TodoItems";
+import "./TodoList.css";
+
+class TodoList extends Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            items: []
+        };
+
+        this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+    }
+    render() {
+        return (
+            <div className="todoListMain">
+                <div className="header">
+                    <form onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a}
+                            placeholder="Enter your task">
+                        </input>
+                        <button type="submit">Save</button>
+                    </form>
+                </div>
+                <TodoItems entries={this.state.items}
+                           delete={this.deleteItem}/>
+            </div>
+        );
+    }
+    addItem(e) {
+        var itemArray = this.state.items;
+
+        if (this._inputElement.value !== "") {
+            itemArray.unshift({
+                text: this._inputElement.value,
+                key: Date.now()
+            });
+
+            this.setState({
+                items: itemArray
+            });
+
+            this._inputElement.value = "";
+        }
+
+        console.log(itemArray);
+
+        e.preventDefault();
+    }
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+}
+
+export default TodoList;
